740996b8ca747a6adf81fc049b28585e9cc6574a
Kaia <danielloewito@gmail.com>
2023-07-05 16:13:58 +1000

    started work on tests

---
+++ test00.sh
@@ -0,0 +1,66 @@
+#! /usr/bin/env dash
+
+# ==============================================================================
+# test00.sh
+# Test the pigs-add command.
+#
+# Written by: Dylan Brotherston <d.brotherston@unsw.edu.au>
+# Date: 2022-06-20
+# For COMP2041/9044 Assignment 1
+# ==============================================================================
+
+# add the current directory to the PATH so scripts
+# can still be executed from it after we cd
+
+PATH="$PATH:$(pwd)"
+
+# Create a temporary directory for the test.
+test_dir="$(mktemp -d)"
+cd "$test_dir" || exit 1
+
+# Create some files to hold output.
+
+expected_output="$(mktemp)"
+actual_output="$(mktemp)"
+
+# Remove the temporary directory when the test is done.
+
+trap 'rm "$expected_output" "$actual_output" -rf "$test_dir"' INT HUP QUIT TERM EXIT
+
+# CREATE PIGS REPO
+
+cat > "$expected_output" <<EOF
+Initialized empty pigs repository in .pig
+EOF
+
+pigs-init > "$actual_output" 2>&1
+# Sanitize the output, removing the full path.
+sed -i 's|^.*/||' "$actual_output"
+
+if ! diff "$expected_output" "$actual_output"; then
+    echo "Failed test"
+    exit 1
+fi
+
+# CHECK IT EXISTS
+
+if [ ! -d ".pig" ]; then
+    echo "Failed test: You didn't actually make a pigs repo LOL"
+    exit 1
+fi
+
+# CREATE SECOND PIGS REPO
+
+cat > "$expected_output" <<EOF
+pigs-init: error: .pig already exists
+EOF
+
+pigs-init > "$actual_output" 2>&1
+sed -i 's|^.*/||' "$actual_output"
+
+if ! diff "$expected_output" "$actual_output"; then
+    echo "Failed test"
+    exit 1
+fi
+
+echo "$0: pigs-init test passed!"
---
+++ test01.sh
@@ -0,0 +1,43 @@
+#! /usr/bin/env dash
+
+# ==============================================================================
+# test00.sh
+# Test the pigs-add command.
+#
+# Written by: Dylan Brotherston <d.brotherston@unsw.edu.au>
+# Date: 2022-06-20
+# For COMP2041/9044 Assignment 1
+# ==============================================================================
+
+# add the current directory to the PATH so scripts
+# can still be executed from it after we cd
+
+PATH="$PATH:$(pwd)"
+
+# Create a temporary directory for the test.
+test_dir="$(mktemp -d)"
+cd "$test_dir" || exit 1
+
+# Create some files to hold output.
+
+expected_output="$(mktemp)"
+actual_output="$(mktemp)"
+
+# Remove the temporary directory when the test is done.
+
+trap 'rm "$expected_output" "$actual_output" -rf "$test_dir"' INT HUP QUIT TERM EXIT
+
+# Create pigs repository
+
+cat > "$expected_output" <<EOF
+Initialized empty pigs repository in .pig
+EOF
+
+pigs-init > "$actual_output" 2>&1
+
+if ! diff "$expected_output" "$actual_output"; then
+    echo "Failed test"
+    exit 1
+fi
+
+echo "$0 test for pigs-add passed!"
